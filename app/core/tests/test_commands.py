df3704dae9680832dfeadace4821a23b3721cecb
Luis Ron <luisron2007@gmail.com>
2022-11-10 15:08:14 +0100

    fix lint

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -21,7 +21,6 @@ class CommandTests(SimpleTestCase):
 
         patched_check.assert_called_once_with(databases=['default'])
 
-
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for db when getting OperationalError"""
