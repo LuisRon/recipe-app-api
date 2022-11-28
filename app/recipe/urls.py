c85eab4bc6e970330e223aa840e970097794a568
Luis Ron <luisron2007@gmail.com>
2022-11-28 15:47:28 +0100

    adding image api

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -11,7 +11,9 @@ from recipe import views
 
 
 router = DefaultRouter()
-router.register('/recipes', views.RecipeViewSet)
+router.register('recipes', views.RecipeViewSet)
+router.register('tags', views.TagViewSet)
+router.register('ingredients', views.IngredientViewSet)
 
 app_name = 'recipe'
 
